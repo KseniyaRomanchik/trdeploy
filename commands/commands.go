b6c17cf31a579b865ee58c63461a3f216f360a3a
Kseniya Romanchik <ks.romanchik@gmail.com>
2020-01-17 15:29:58 +0300

    Added loading flags from config, moved flags and commands names to constatns

--- commands/commands.go
+++ commands/commands.go
@@ -3,47 +3,61 @@ package commands
 import (
 	"fmt"
 	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
 	"trdeploy/flags"
 )
 
-var Commands = []*cli.Command{
-	{
-		Name:      "init",
-		Aliases:   []string{"i"},
-		UsageText: "*** init ",
-		Usage:     "init",
-		Flags:     flags.Flags,
-		Action:    commandAction(InitAction),
-	},
-	{
-		Name:      "plan",
-		Aliases:   []string{"p"},
-		UsageText: "*** plan ",
-		Usage:     "plan",
-		Flags:     flags.Flags,
-		Action:    commandAction(InitAction, Plan),
-	},
-	{
-		Name:      "apply",
-		Aliases:   []string{"a"},
-		UsageText: "*** apply ",
-		Usage:     "apply",
-		Flags:     flags.Flags,
-		Action:    commandAction(InitAction, Apply),
-	},
-	{
-		Name:      "destroy",
-		Aliases:   []string{"d"},
-		UsageText: "*** destroy ",
-		Usage:     "destroy",
-		Flags:     flags.Flags,
-		Action:    commandAction(InitAction, Destroy),
-	},
+var (
+	Commands []*cli.Command
+)
+
+func LoadCommands() {
+	Commands = []*cli.Command{
+		{
+			Name:      Init,
+			Aliases:   []string{"i"},
+			UsageText: "*** init ",
+			Usage:     "init",
+			Before:    beforeAction,
+			Flags:     flags.Flags,
+			Action:    commandAction(initAction),
+		},
+		{
+			Name:      Plan,
+			Aliases:   []string{"p"},
+			UsageText: "*** plan ",
+			Usage:     "plan",
+			Before:    beforeAction,
+			Flags:     flags.Flags,
+			Action:    commandAction(initAction, plan),
+		},
+		{
+			Name:      Apply,
+			Aliases:   []string{"a"},
+			UsageText: "*** apply ",
+			Usage:     "apply",
+			Before:    beforeAction,
+			Flags:     flags.Flags,
+			Action:    commandAction(initAction, apply),
+		},
+		{
+			Name:      Destroy,
+			Aliases:   []string{"d"},
+			UsageText: "*** destroy ",
+			Usage:     "destroy",
+			Before:    beforeAction,
+			Flags:     flags.Flags,
+			Action:    commandAction(initAction, destroy),
+		},
+	}
 }
 
 func commandAction(actionFns ...func(c *cli.Context) error) func(c *cli.Context) error {
 	return func(c *cli.Context) error {
 		fmt.Printf("\n %s %s\n", c.Command.UsageText, CurrentDir())
+		for _, f := range c.App.VisibleFlags() {
+			fmt.Printf("\t *  %s: %+v\n", f.Names()[0], c.String(f.Names()[0]))
+		}
 
 		for _, fn := range actionFns {
 			if err := fn(c); err != nil {
@@ -53,4 +67,22 @@ func commandAction(actionFns ...func(c *cli.Context) error) func(c *cli.Context)
 
 		return nil
 	}
+}
+
+func beforeAction(c *cli.Context) error {
+	replaceModuleTfvars(c)
+
+	return altsrc.InitInputSourceWithContext(flags.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))(c)
+}
+
+func replaceModuleTfvars(c *cli.Context) {
+	mtv := c.String(flags.ModuleTfvars)
+	newMtv := fmt.Sprintf("var/%s", mtv)
+
+	if c.IsSet(flags.ModuleTfvars) {
+		wp := c.String(flags.WorkProfile)
+		newMtv = fmt.Sprintf("var/%s.tfvars", wp)
+	}
+
+	c.Set(flags.ModuleTfvars, newMtv)
 }
